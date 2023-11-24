a221f9d06b78c034c7e140d11e66739a0f3f2214
Alexander Bulatov <aleks40k@yandex.ru>
2023-11-24 14:02:14 +0300

    Add custom ArrayList and LinkedList implementations with test coverage

--- src/main/java/ru/expanse/custom/collection/CustomList.java
+++ src/main/java/ru/expanse/custom/collection/CustomList.java
@@ -1,2 +1,59 @@
-package ru.expanse.custom.collection;public interface CustomList {
+package ru.expanse.custom.collection;
+
+import java.util.Comparator;
+
+public interface CustomList<E> {
+    /**
+     * Add new element to the list.
+     *
+     * @param elem - added element.
+     */
+    void add(E elem);
+
+    /**
+     * Add new element at the specified index.
+     *
+     * @param elem - added element.
+     * @param idx  - target index.
+     */
+    void add(E elem, int idx);
+
+    /**
+     * Return element located at the specified index.
+     *
+     * @param idx - target index.
+     * @return E - returned element.
+     */
+    E get(int idx);
+
+    /**
+     * Return and remove element located at the specified index.
+     *
+     * @param idx - target index.
+     * @return E - removed element.
+     */
+    E remove(int idx);
+
+    /**
+     * Remove all elements from the list.
+     */
+    void clear();
+
+    /**
+     * Sort the list according to the supplied comparator's logic.
+     */
+    void sort(Comparator<E> comparator);
+
+    /**
+     * Return an array of the requested type containing elements of this list.
+     *
+     * @param target - supplied (empty) array of the requested type.
+     * @return T[] - resulting array of the requested type.
+     */
+    <T> T[] toArray(T[] target);
+
+    /**
+     * Return the size of the list.
+     */
+    int size();
 }
---
+++ src/test/java/ru/expanse/custom/collection/impl/CustomLinkedListTest.java
@@ -0,0 +1,91 @@
+package ru.expanse.custom.collection.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomLinkedListTest {
+    private CustomLinkedList<Integer> list;
+
+    @BeforeEach
+    void refreshList() {
+        list = new CustomLinkedList<>();
+    }
+
+    @Test
+    void add() {
+        Integer[] arr = getDefaultArray();
+        fillList(list, arr);
+        assertArrayEquals(arr, list.toArray(new Integer[0]));
+    }
+
+    @Test
+    void addByIdx() {
+        Integer[] arr1 = new Integer[]{3, 20, 10};
+        fillList(list, arr1);
+
+        list.add(-5, 1);
+        Integer[] arr2 = new Integer[]{3, -5, 20, 10};
+        assertArrayEquals(arr2, list.toArray(new Integer[0]));
+
+        list.add(2, 3);
+        Integer[] arr3 = new Integer[]{3, -5, 20, 2, 10};
+        assertArrayEquals(arr3, list.toArray(new Integer[0]));
+    }
+
+    @Test
+    void throwsExceptionForAddingIndexOutOfBounds() {
+        assertThrows(IndexOutOfBoundsException.class, () -> list.add(5, 0));
+    }
+
+    @Test
+    void remove() {
+        Integer[] arr1 = new Integer[]{3, -5, 20, 10, 2};
+        fillList(list, arr1);
+
+        assertEquals(arr1[1], list.remove(1));
+        Integer[] arr2 = new Integer[]{3, 20, 10, 2};
+        assertArrayEquals(arr2, list.toArray(new Integer[0]));
+
+        assertEquals(arr2[3], list.remove(3));
+        Integer[] arr3 = new Integer[]{3, 20, 10};
+        assertArrayEquals(arr3, list.toArray(new Integer[0]));
+    }
+
+    @Test
+    void clear() {
+        fillList(list, getDefaultArray());
+        list.clear();
+        assertEquals(0, list.size());
+        assertThrows(IndexOutOfBoundsException.class, () -> list.get(0));
+    }
+
+    @Test
+    void sort() {
+        Integer[] arr = getDefaultArray();
+        fillList(list, arr);
+
+        Arrays.sort(arr);
+        list.sort(Comparator.naturalOrder());
+        assertArrayEquals(arr, list.toArray(new Integer[0]));
+    }
+
+    @Test
+    void toArray() {
+        Integer[] arr = getDefaultArray();
+        fillList(list, arr);
+        assertArrayEquals(arr, list.toArray(new Integer[0]));
+    }
+
+    private void fillList(CustomLinkedList<Integer> list, Integer[] arr) {
+        Arrays.stream(arr).forEach(list::add);
+    }
+
+    private Integer[] getDefaultArray() {
+        return new Integer[]{3, -5, 20, 10, 2};
+    }
+}
